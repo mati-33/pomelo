d482564c43a4dffc955a9b85ae76107bee7a3483
mati <mateusz.meksula@gmail.com>
2026-01-24 19:25:18 +0100

    add and delete list

--- models/lists.go
+++ models/lists.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbletea"
 
 	"pomelo/data"
+)
+
+type mode int
+
+const (
+	listMode mode = iota
+	addMode
+	deleteMode
 )
 
 type item struct {
@@ -21,9 +30,12 @@ func (i item) Description() string {
 func (i item) FilterValue() string { return i.Name }
 
 type listsScreen struct {
-	list list.Model
-	err  error
-	db   *sql.DB
+	list        list.Model
+	addInput    textinput.Model
+	deleteInput textinput.Model
+	mode        mode
+	err         error
+	db          *sql.DB
 }
 
 func newListsScreen(db *sql.DB) listsScreen {
@@ -32,57 +44,173 @@ func newListsScreen(db *sql.DB) listsScreen {
 	l.FilterInput.Prompt = "/"
 	l.SetShowHelp(false)
 	l.SetShowStatusBar(false)
+	l.DisableQuitKeybindings()
+
+	ai := textinput.New()
+	ai.Prompt = ""
+	di := textinput.New()
+	di.Prompt = ""
+	di.CharLimit = 1
 
 	return listsScreen{
-		list: l,
-		db:   db,
+		list:        l,
+		addInput:    ai,
+		deleteInput: di,
+		mode:        listMode,
+		db:          db,
 	}
 }
 
 type LoadResult struct {
-	lists []data.List
+	lists []list.Item
 	err   error
 }
 
+type ListAdded struct{}
+
+type ListDeleted struct{}
+
 func (m listsScreen) Init() tea.Cmd {
-	return func() tea.Msg {
-		lists, err := data.GetAllLists(m.db)
-		return LoadResult{lists, err}
-	}
+	return GetAllListsCmd(m.db)
 }
 
 func (m listsScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	case ListAdded, ListDeleted:
+		return m, GetAllListsCmd(m.db)
+
 	case LoadResult:
 		if msg.err != nil {
 			m.err = msg.err
 			return m, nil
 		}
 
-		items := make([]list.Item, 0, len(msg.lists))
-		for _, l := range msg.lists {
-			items = append(items, item{l})
-		}
-		cmd := m.list.SetItems(items)
+		cmd := m.list.SetItems(msg.lists)
 		return m, cmd
 
 	case tea.WindowSizeMsg:
 		m.list.SetSize(msg.Width, msg.Height-3)
+
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "a":
-		case "d":
+		switch m.mode {
+		case listMode:
+			if m.list.FilterState() != list.Filtering {
+				switch msg.String() {
+				case "a":
+					m.mode = addMode
+					m.list.SetShowTitle(false)
+					return m, m.addInput.Focus()
+				case "d":
+					m.mode = deleteMode
+					m.list.SetShowTitle(false)
+					return m, m.deleteInput.Focus()
+				}
+			}
+
+		case deleteMode:
+			switch msg.String() {
+			case "enter":
+				value := m.deleteInput.Value()
+				if value == "n" {
+					m.mode = listMode
+					m.list.SetShowTitle(true)
+					m.deleteInput.SetValue("")
+					m.deleteInput.Blur()
+				}
+				if value == "y" {
+					m.mode = listMode
+					m.list.SetShowTitle(true)
+					m.deleteInput.SetValue("")
+					m.deleteInput.Blur()
+					id := m.list.SelectedItem().(item).ID
+					return m, func() tea.Msg {
+						err := data.DeleteList(m.db, id)
+						if err != nil {
+							return nil
+						}
+						return ListDeleted{}
+					}
+
+				}
+			case "esc":
+				m.mode = listMode
+				m.list.SetShowTitle(true)
+				m.deleteInput.SetValue("")
+				m.deleteInput.Blur()
+			}
+
+		case addMode:
+			switch msg.String() {
+			case "enter":
+				name := m.addInput.Value()
+				if len(name) > 0 {
+					m.mode = listMode
+					m.list.SetShowTitle(true)
+					m.addInput.SetValue("")
+					m.addInput.Blur()
+					return m, func() tea.Msg {
+						_, err := data.AddList(m.db, name)
+						if err != nil {
+							return nil
+						}
+						return ListAdded{}
+					}
+				}
+			case "esc":
+				m.mode = listMode
+				m.list.SetShowTitle(true)
+				m.addInput.SetValue("")
+				m.addInput.Blur()
+			}
 		}
 	}
 
 	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
+	switch m.mode {
+	case listMode:
+		m.list, cmd = m.list.Update(msg)
+	case addMode:
+		m.addInput, cmd = m.addInput.Update(msg)
+	case deleteMode:
+		m.deleteInput, cmd = m.deleteInput.Update(msg)
+	}
 	return m, cmd
+
 }
 
 func (m listsScreen) View() string {
 	if m.err != nil {
 		return fmt.Sprintf("Error!!: %v", m.err)
 	}
-	return m.list.View()
+
+	ret := ""
+
+	switch m.mode {
+
+	case deleteMode:
+		ret += "delete? (y/n): " + m.deleteInput.View() + "\n"
+
+	case addMode:
+		ret += "name: " + m.addInput.View() + "\n"
+	}
+
+	ret += m.list.View()
+	return ret
+}
+
+func GetAllListsCmd(db *sql.DB) tea.Cmd {
+	return func() tea.Msg {
+		lists, err := data.GetAllLists(db)
+		if err != nil {
+			return LoadResult{[]list.Item{}, err}
+		}
+
+		items := make([]list.Item, 0, len(lists))
+		for _, l := range lists {
+			items = append(items, item{l})
+		}
+
+		return LoadResult{items, err}
+	}
 }
